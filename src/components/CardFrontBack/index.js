ac3247b641822f0b2df30e1b3d28f4922e318ee9
vinicius morais <vmorais194@gmail.com>
2022-02-02 22:36:59 -0300

    Add flipcard feature

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,10 +1,15 @@
+import './style.css'
 import CardGame from "../CardGame"
 
 function CardFrontBack(){
     return `
         <article class="card-front-back">
-        ${CardGame()}
-        ${CardGame("javascript", "logo javascript")}
+            <div class="front">
+                ${CardGame()}
+            </div>
+            <div class="back">
+                ${CardGame("javascript", "logo javascript")}
+            </div>
         </article>
     `
 }
